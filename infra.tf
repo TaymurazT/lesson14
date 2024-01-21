896da64ef658187a58ed941c96244d353169aa58
Taymuraz Tebiev <ttebiev@gmail.com>
2024-01-21 16:07:51 +0300

    imaage id

--- infra.tf
+++ infra.tf
@@ -8,7 +8,7 @@ resource "yandex_compute_instance" "vm-1" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd87va5cc00gaq2f5qfb"
+      image_id = "fd8bu9gsckcm2351kqaq"
     }
   }
 
@@ -32,7 +32,7 @@ resource "yandex_compute_instance" "vm-2" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd87va5cc00gaq2f5qfb"
+      image_id = "fd8bu9gsckcm2351kqaq"
     }
   }
 
