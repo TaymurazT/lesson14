372e96c004352936c615ab2e288c4528b33d38dd
Taymuraz Tebiev <ttebiev@gmail.com>
2024-01-21 15:48:53 +0300

    copy privider config

--- infra.tf
+++ infra.tf
@@ -1,3 +1,24 @@
+terraform {
+#  required_version = "1.1.7"
+
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.105.0"
+    }
+  }
+}
+
+provider "yandex" {
+    token  = "y0_AgAAAAANhruJAATuwQAAAAD4r9GtCfNVfAtkQLyDf-36uo9fEWuYSnY"
+    cloud_id  = "b1g3kq4jkid5coa26uk9"
+    folder_id  = "b1gaqd9fid4ptghu9b80"
+    zone = "ru-central1-b"
+}
+
+resource "yandex_compute_instance" "lesson14" {
+  name = "terraform1"
+}
 
 resource "yandex_compute_instance" "vm-1" {
   name = "terraform1"
