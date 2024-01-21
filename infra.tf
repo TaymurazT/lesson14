e378b99a99383127758c1ea6645940c173e7adf0
Taymuraz Tebiev <ttebiev@gmail.com>
2024-01-21 16:26:20 +0300

    many changes

--- infra.tf
+++ infra.tf
@@ -18,7 +18,8 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("/home/allon/.ssh/id_rsa.pub")}"
+     user-data = "${file("/home/allon/lesson14/lesson14/meta.txt")}"
+ #   ssh-keys = "ubuntu:${file("/home/allon/.ssh/id_rsa.pub")}"
   }
 }
 
@@ -42,7 +43,8 @@ resource "yandex_compute_instance" "vm-2" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("/home/allon/.ssh/id_rsa.pub")}"
+     user-data = "${file("/home/allon/lesson14/lesson14/meta.txt")}"
+#    ssh-keys = "ubuntu:${file("/home/allon/.ssh/id_rsa.pub")}"
   }
 }
 
@@ -74,42 +76,3 @@ output "external_ip_address_vm_2" {
 }
 
 
-#data "yandex_compute_image" "ubuntu_image" {
-#  family = "ubuntu-2004-lts"
-#}
- 
-#resource "yandex_compute_instance" "build" {
-#  name = "build"
- 
-#  resources {
-#    cores  = 2
-#    memory = 2
-#  }
- 
-#  boot_disk {
-#    initialize_params {
-#      image_id = data.yandex_compute_image.ubuntu_image.id
-#    }
-#  }
- 
-#  network_interface {
- #   subnet_id = yandex_vpc_subnet.default-ru-central1-b.id
-#    nat       = true
-#  }
- 
-#  metadata = {
-#    user-data = "${file("./meta.yml")}"
-#  }
- 
-#}
- 
-#resource "yandex_vpc_network" "default-ru-central1-b" {
-#  name = "default-ru-central1-b"
-#}
- 
-#resource "yandex_vpc_subnet" "default-ru-central1-b" {
-#  name           = "default-ru-central1-b"
-#  zone           = "ru-central1-b"
-#  network_id     = yandex_vpc_network.default-ru-central1-b.id
-#  v4_cidr_blocks = ["192.168.15.0/24"]
-#}
