5a03e6e201036ff1ce9cc2baf3f4b465549d16d6
Taymuraz Tebiev <ttebiev@gmail.com>
2024-01-21 15:05:39 +0300

    comment #

--- infra.tf
+++ infra.tf
@@ -16,10 +16,10 @@ resource "yandex_compute_instance" "build" {
     }
   }
  
-  network_interface {
-    subnet_id = yandex_vpc_subnet.default-ru-central1-b.id
-    nat       = true
-  }
+#  network_interface {
+ #   subnet_id = yandex_vpc_subnet.default-ru-central1-b.id
+#    nat       = true
+#  }
  
   metadata = {
     user-data = "${file("./meta.yml")}"
@@ -27,12 +27,13 @@ resource "yandex_compute_instance" "build" {
  
 }
  
-resource "yandex_vpc_network" "default-ru-central1-b" {
-  name = "default-ru-central1-b"
-}
+#resource "yandex_vpc_network" "default-ru-central1-b" {
+#  name = "default-ru-central1-b"
+#}
  
-resource "yandex_vpc_subnet" "default-ru-central1-b" {
-  name           = "default-ru-central1-b"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.default-ru-central1-b.id
-}
+#resource "yandex_vpc_subnet" "default-ru-central1-b" {
+#  name           = "default-ru-central1-b"
+#  zone           = "ru-central1-b"
+#  network_id     = yandex_vpc_network.default-ru-central1-b.id
+#  v4_cidr_blocks = ["192.168.15.0/24"]
+#}
