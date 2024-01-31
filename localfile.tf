1cfb226a2c4f3107b661f9f5cce6f14180c7300f
Taymuraz Tebiev <ttebiev@gmail.com>
2024-01-31 22:14:03 +0300

    localfile

---
+++ localfile.tf
@@ -0,0 +1,14 @@
+resource "local_file" "inventory" {
+  filename = "/etc/ansible/hosts"
+  file_permission  = "0644"
+
+  content = templatefile("${path.module}/inventory.tpl", {
+    #ip_addrs = google_compute_instance.andrdi-gcp-server[*].network_interface[0].access_config[0].nat_ip
+    ip_addrs = yandex_compute_instance.vm-[*].network_interface.[0].ip_address
+  })
+
+ # provisioner "local-exec" {
+  #  command = "ANSIBLE_CONFIG=${path.module}/../ansible/ansible.cfg ansible-playbook ${path.module}/../ansible/site.yml"
+  #}
+}
+
