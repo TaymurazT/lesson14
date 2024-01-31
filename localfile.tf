1e1616958f572c15412b4f3ed3d58a95f1531211
Taymuraz Tebiev <ttebiev@gmail.com>
2024-01-31 22:37:53 +0300

    ss

--- localfile.tf
+++ localfile.tf
@@ -5,7 +5,7 @@ resource "local_file" "inventory" {
   content = templatefile("/home/allon/lesson14/lesson14/inventory.tpl", {
     #ip_addrs = google_compute_instance.andrdi-gcp-server[*].network_interface[0].access_config[0].nat_ip
     ip_addrs = yandex_compute_instance.vm-1.network_interface.0.ip_address
-    ip_addrs = yandex_compute_instance.vm-2.network_interface.0.ip_address
+    #ip_addrs = yandex_compute_instance.vm-2.network_interface.0.ip_address
   })
 
  # provisioner "local-exec" {
