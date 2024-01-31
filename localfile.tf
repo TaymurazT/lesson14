1e42487c5245ff516e01f0f2442f3f0b7bf451ff
Taymuraz Tebiev <ttebiev@gmail.com>
2024-01-31 22:35:56 +0300

    /home/allon/lesson14/lesson14

--- localfile.tf
+++ localfile.tf
@@ -2,7 +2,7 @@ resource "local_file" "inventory" {
   filename = "/etc/ansible/hosts"
   file_permission  = "0644"
 
-  content = templatefile("${path.module}/inventory.tpl", {
+  content = templatefile("/home/allon/lesson14/lesson14/inventory.tpl", {
     #ip_addrs = google_compute_instance.andrdi-gcp-server[*].network_interface[0].access_config[0].nat_ip
     ip_addrs = yandex_compute_instance.vm-1.network_interface.0.ip_address
     ip_addrs = yandex_compute_instance.vm-2.network_interface.0.ip_address
